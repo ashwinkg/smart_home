b39d2fbbba9cc2c65541c892189405b9b0c17661
Ashwin <ashu.ashwinkg@gmail.com>
2023-09-13 00:53:06 -0700

    made connection between login and home

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (e) => {
+    console.log("Logging in...");
+
+    // Navigate to the admin page after successful login.
+    navigate("/admin");
+  };
+
+  const classes = {
+    pageBody: "h-screen flex bg-gray-bg1",
+    formContainer:
+      "w-full max-w-md    m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16",
+    formHeading: "text-2xl  font-medium text-primary mt-4 mb-12 text-center",
+    btnContainer: "flex justify-center items-center mt-6",
+  };
+
+  return (
+    <div className={classes.pageBody}>
+      <div className={classes.formContainer}>
+        <h1 className={classes.formHeading}>Log in to your account</h1>
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              className="w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              id="email"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              className="w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              id="password"
+              placeholder="Your Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center items-center mt-6">
+            <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
